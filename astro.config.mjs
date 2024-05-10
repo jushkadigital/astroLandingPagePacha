2924edc7aef994da9f02b51e1f5c1ab7dc95ebc7
chamba <urgosxd@gmail.com>
2024-05-10 07:35:02 -0700

    aoe

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   image: {
-    domains: ["ik.imagekit.io",'res.cloudinary.com']
+    domains: ["ik.imagekit.io", 'res.cloudinary.com']
   },
   output: "server",
   adapter: netlify()
---
+++ src/pages/videos/pacha8.md
@@ -0,0 +1,5 @@
+---
+video1: 'https://youtu.be/96SNMK1ylFs?si=fWyTrOmB6-2XsT6q'
+video2: 'https://youtu.be/KJTQOOSPceo?si=-e2xaXANOPUVnrAd'
+video3: 'https://youtu.be/0Zpz2npJ56k?si=LTUiRE9hynf-XCVg'
+---
