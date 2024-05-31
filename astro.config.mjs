0e64a5ae0d8c606cbd007125b1d55dc4c545b5f5
chamba <urgosxd@gmail.com>
2024-05-31 08:47:50 -0700

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
+  integrations: [tailwind(), react(), mdx(), partytown()],
   image: {
     domains: ["ik.imagekit.io", 'res.cloudinary.com']
   },
