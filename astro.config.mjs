ee7c07053ac103591753fcc403de44e074b3158c
chamba <urgosxd@gmail.com>
2024-06-05 09:50:45 -0700

    aoe

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,9 @@ import react from "@astrojs/react";
 import netlify from "@astrojs/netlify";
 import mdx from "@astrojs/mdx";
 
-import partytown from "@astrojs/partytown";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx(), partytown({
-  config: {
-      resolveUrl: function(url) {
-    if (url.hostname === "connect.facebook.net") {
-      var proxyUrl = new URL('https://my-reverse-proxy.com/');
-      proxyUrl.searchParams.append('url', url.href);
-      return proxyUrl;
-    }
-    return url;
-  },
-			  forward: ["dataLayer.push","fbq"],
-			},
-  })],
+  integrations: [tailwind(), react(), mdx()],
   image: {
     domains: ["ik.imagekit.io", 'res.cloudinary.com']
   },
