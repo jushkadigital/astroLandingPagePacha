bb11c60492ec5901c51838381fb94ae7ae833c86
chamba <urgosxd@gmail.com>
2024-06-27 07:12:44 -0700

    aoe

--- functions/bitrix.js
+++ functions/bitrix.js
@@ -46,9 +46,10 @@ function parseMultipartForm(event) {
 
 exports.handler = async (event,context) => {
  console.log("aoeaoe") 
-const fields = await parseMultipartForm(event)
-  console.log(fields)
+// const fields = await parseMultipartForm(event)
+  // console.log(fields)
   
+  console.log(JSON.parse(event.body))
   return {
     statusCode:200,
     body: JSON.stringify({message: `gaa`})
