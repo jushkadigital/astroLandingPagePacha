6625667b83ff3c19a6d4b5b5f00a1f82e693c766
chamba <urgosxd@gmail.com>
2024-06-27 08:56:58 -0700

    aoe

--- functions/bitrix.js
+++ functions/bitrix.js
@@ -1,4 +1,6 @@
 const axios = require("axios")
+const qs = require('qs')
+
 exports.handler = async (event,context) => {
  console.log("aoeaoe") 
 // const fields = await parseMultipartForm(event)
@@ -10,19 +12,22 @@ exports.handler = async (event,context) => {
   try{
   const currentDateTime = new Date()
   const formatedDateTime = currentDateTime.toISOString()
-  const params = new URLSearchParams()
-  params.append('FIELDS[TITLE]',"Formulario Landing Page Pachas")
-  params.append('FIELDS[UF_CRM_LEAD_1712673755506]',data.month)
-  params.append('FIELDS[DATE_CREATE]',formatedDateTime)
-  // params.append('FIELDS[ADDRESS_COUNTRY]',rawFormData.country)
-  params.append('FIELDS[NAME]',data.nombres)
-  params.append('FIELDS[LAST_NAME]',data.apellidos)
-  params.append('FIELDS[EMAIL][0][VALUE]',data.correo)
-  params.append('FIELDS[EMAIL][0][VALUE_TYPE]','Work')
-  params.append('FIELDS[PHONE][0][VALUE]',`+`+data.phoneCode +' '+data.telefono)
-  params.append('FIELDS[PHONE][0][VALUE_TYPE]','Work')
+    const params = {
 
-  axios.post('https://pdscorporation.bitrix24.es/rest/108335/vfbji7p1qmfx0m7d/crm.lead.add.json',params , { headers:{
+    }
+    
+  params['FIELDS[TITLE]']  = "Formulario Landing Page Pachas"
+  params['FIELDS[UF_CRM_LEAD_1712673755506]'] = data.month
+  params['FIELDS[DATE_CREATE]'] = formatedDateTime
+  // params.append('FIELDS[ADDRESS_COUNTRY]',rawFormData.country)
+  params['FIELDS[NAME]']  = data.nombres
+  params['FIELDS[LAST_NAME]'] = data.apellidos
+  params['FIELDS[EMAIL][0][VALUE]'] = data.correo
+  params['FIELDS[EMAIL][0][VALUE_TYPE]'] ='Work'
+  params['FIELDS[PHONE][0][VALUE]'] = `+`+data.phoneCode +' '+data.telefono
+  params['FIELDS[PHONE][0][VALUE_TYPE]'] = 'Work'
+
+  axios.post('https://pdscorporation.bitrix24.es/rest/108335/vfbji7p1qmfx0m7d/crm.lead.add.json',qs.stringify(params) , { headers:{
       'Content-Type': 'application/x-www-form-urlencoded'
     } })
   .then(response => {
